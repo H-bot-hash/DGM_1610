db0ee464775a609b22c813396dced25047287e97
Hgerrits <hank658@rocketmail.com>
2021-06-11 11:32:30 -0600

    Variables and Operators

---
+++ Operators.cs
@@ -0,0 +1,33 @@
+
+
+
+int myNum = 60;
+int myNum2 = 30;
+double myFloat = 20.0;
+
+myFloat -= 8;
+myNum2 * 2;
+myFloat ++;
+
+
+
+
+Console.WriteLine("The value of number 1 is now : " + myNum);
+Console.WriteLine("The value of number 2 is now : " + myNum2);\
+Console.WriteLine("The value of number 3 is now : " + myFloat);
+
+
+
+ 
+
+if (myNum2 == myNum)
+
+{
+
+ Console.WriteLine("The values of number 1 and 2 are even!"); 
+
+{
+
+
+Console.WriteLine(myNum == 60 && myNum2 == 60);
+
---
+++ Variables.cs
@@ -0,0 +1,19 @@
+
+
+int x = 5;
+double y = 2.3;
+char z = 'a';
+string line = "Hello World";
+bool program = true;
+
+
+Console.WriteLine(line);
+Console.WriteLine("This is an int vaule: " + x);
+Console.WriteLine("This is an double value: " + y);
+Console.WriteLine("This is a char value: " + z);
+
+if (bool program == true)
+{
+    Console.WriteLine("The program is running");
+}
+
